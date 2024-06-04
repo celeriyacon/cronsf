c39174726190d7cf5eee57fb49ec809d463bf163
celeriyacon <celeriyacon@users.noreply.github.com>
2024-06-04 13:55:17 -0700

    Add missing copyright and license notices, and remove them from the standard library header files that don't contain any code.

--- gs/scsp.h
+++ gs/scsp.h
@@ -1,7 +1,7 @@
 /*
  * gs/scsp.h
  *
- * Copyright (C) 2021 celeriyacon - https://github.com/celeriyacon
+ * Copyright (C) 2021-2024 celeriyacon - https://github.com/celeriyacon
  *
  * This software is provided 'as-is', without any express or implied
  * warranty.  In no event will the authors be held liable for any damages
